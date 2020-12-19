c181669d598bf9cc971186e830c8dd8516825f6a
marwabejaoui <73910175+marwabejaoui@users.noreply.github.com>
2020-12-19 20:20:22 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MyName from './component/Profile/MyName';
+import MyPhoto from './component/Profile/MyPhoto';
+import MyAddress from './component/Profile/MyAddress';
+const App = () => (
+ <>
+    < MyPhoto />
+    < MyName />
+    < MyAddress/>
+ </>
+);
+export default App;
